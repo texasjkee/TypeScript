374223d19b1fa238d9abfddaa5405b64d9254f1c
Michael <gt.gluck@gmail.com>
2024-03-09 16:25:58 +0200

    add: Literal_types.ts

---
+++ TS/Literal_types.ts
@@ -0,0 +1,9 @@
+let msg: 'Hello' = 'Hello';
+msg = 'Hello';
+
+const port3000: number = 3000;
+const port3001: number = 3001;
+
+function startServer(protocol: 'http' | 'https', port: 3000 | 3001): 'Server is started' {
+  return 'Server is started';
+}
