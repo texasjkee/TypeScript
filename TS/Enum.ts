7eaab488e9596f6e2213c94a079bdc37033d0f16
Michael <gt.gluck@gmail.com>
2024-03-12 16:29:20 +0200

    add: Enam.ts

---
+++ TS/Enum.ts
@@ -0,0 +1,20 @@
+enum Directions {
+  TOP,
+  RIGHT,
+  LEFT,
+  BOTTOM
+}
+
+enum TimingFunc {
+  EASE,
+  EASE_IN,
+  LINEAR
+}
+
+function frame (elem: string, dir: Directions, tFunc: string): void {
+  if(dir === Directions.RIGHT) {
+
+  }  
+}
+
+frame('id', Directions.RIGHT, 'fast')
